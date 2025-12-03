4b04bdf8aec2ce796fd043ce2afb389bea31baff
Felipe Ruiz Terrazas <feliperterrazas@gmail.com>
2025-12-03 11:59:11 -0300

    feat: added new hooks

--- resources/js/i18n/react/index.ts
+++ resources/js/i18n/react/index.ts
@@ -1,3 +1,6 @@
+export { useGetLocale } from './hooks/useGetLocale';
+export { useSetLocale } from './hooks/useSetLocale';
+export { useSupportedLocales } from './hooks/useSupportedLocales';
+export { useTranslation } from './hooks/useTranslation';
 export { I18nContext } from './I18nContext';
 export { I18nProvider } from './I18nProvider';
-export { useTranslation } from './useTranslation';
