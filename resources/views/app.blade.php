0fe7053c894141063fcd0620143e9a35bec5397e
Felipe Ruiz Terrazas <feliperterrazas@gmail.com>
2025-12-03 11:54:21 -0300

    feat: add csrf token to blade layout

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -1,11 +1,13 @@
 <!DOCTYPE html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
 
     <title inertia>{{ config('app.name', 'Laravel') }}</title>
+
+    <!-- CSRF token -->
+    <meta name="csrf-token" content="{{ csrf_token() }}">
 
     <!-- Fonts -->
     <link rel="preconnect" href="https://fonts.bunny.net">
@@ -15,16 +17,13 @@
     <link rel="icon" type="image/svg+xml" href="{{ asset('favicon.svg') }}?v=2">
     <link rel="alternate icon" type="image/x-icon" href="{{ asset('favicon.ico') }}?v=2">
 
-
     <!-- Scripts -->
     @routes
     @viteReactRefresh
     @vite(['resources/js/app.tsx', "resources/js/Pages/{$page['component']}.tsx"])
     @inertiaHead
 </head>
-
 <body class="font-sans antialiased">
     @inertia
 </body>
-
 </html>
