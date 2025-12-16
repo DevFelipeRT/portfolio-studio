b7c1b4cbc78730d3ea677ae7f27852a25b98b74a
Felipe Ruiz Terrazas <feliperterrazas@gmail.com>
2025-12-16 14:27:29 -0300

    feat: use RegisterCapabilitiesTrait

--- app/Modules/Experiences/Infrastructure/Providers/ExperiencesServiceProvider.php
+++ app/Modules/Experiences/Infrastructure/Providers/ExperiencesServiceProvider.php
@@ -4,10 +4,8 @@ declare(strict_types=1);
 
 namespace App\Modules\Experiences\Infrastructure\Providers;
 
-use App\Modules\Experiences\Application\Capabilities\Providers\VisibleExperiences;
 use App\Modules\Experiences\Application\Services\ExperienceService;
-use App\Modules\Shared\Contracts\Capabilities\ICapabilitiesFactory;
-use App\Modules\Shared\Contracts\Capabilities\ICapabilityCatalog;
+use App\Modules\Shared\Support\Capabilities\Traits\RegisterCapabilitiesTrait;
 use Illuminate\Support\Facades\Route;
 use Illuminate\Support\ServiceProvider;
 
@@ -16,12 +14,17 @@ use Illuminate\Support\ServiceProvider;
  */
 final class ExperiencesServiceProvider extends ServiceProvider
 {
+    use RegisterCapabilitiesTrait;
+
     /**
      * Register any services.
      */
     public function register(): void
     {
         $this->registerBindings();
+        $this->registerCapabilitiesIfAvailable([
+            ExperienceService::class,
+        ]);
     }
 
     /**
@@ -30,7 +33,7 @@ final class ExperiencesServiceProvider extends ServiceProvider
     public function boot(): void
     {
         $this->registerRoutes();
-        $this->registerCapabilitiesIfAvailable();
+
     }
 
     /**
@@ -55,42 +58,5 @@ final class ExperiencesServiceProvider extends ServiceProvider
         //     ->group(function (): void {
         //         $this->loadRoutesFrom(__DIR__ . '/../../Routes/public.php');
         //     });
-    }
-
-    /**
-     * Attempts to register capabilities in the central catalog if it is bound.
-     */
-    private function registerCapabilitiesIfAvailable(): void
-    {
-        if (!$this->app->bound(ICapabilityCatalog::class)) {
-            return;
-        }
-
-        /** @var ICapabilityCatalog $catalog */
-        $catalog = $this->app->make(ICapabilityCatalog::class);
-
-        $this->registerCapabilities($catalog);
-    }
-
-    /**
-     * Registers this module's capabilities in the central capability catalog.
-     */
-    private function registerCapabilities(ICapabilityCatalog $catalog): void
-    {
-        /** @var ExperienceService $experienceService */
-        $experienceService = $this->app->make(ExperienceService::class);
-
-        /** @var ICapabilitiesFactory $capabilitiesFactory */
-        $capabilitiesFactory = $this->app->make(ICapabilitiesFactory::class);
-
-        $visibleExperiences = new VisibleExperiences(
-            $experienceService,
-            $capabilitiesFactory,
-        );
-
-        $catalog->register(
-            $visibleExperiences->getDefinition(),
-            $visibleExperiences,
-        );
     }
 }
