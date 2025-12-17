a6978939e584b8a1466ca638b3dcef06c8fecd4c
Felipe Ruiz Terrazas <feliperterrazas@gmail.com>
2025-12-17 16:41:51 -0300

    fix: improved logic

--- app/Modules/ContentManagement/Config/templates.php
+++ app/Modules/ContentManagement/Config/templates.php
@@ -10,17 +10,31 @@ declare(strict_types=1);
  *
  * @return array<int,array<string,mixed>>
  */
-$baseDir = __DIR__ . '/Templates';
+$baseDir = dirname(__DIR__) . '/Templates';
+
+if (!is_dir($baseDir)) {
+    throw new RuntimeException(
+        sprintf('ContentManagement templates directory not found: [%s].', $baseDir),
+    );
+}
+
+$files = glob($baseDir . '/*.php') ?: [];
+
+if ($files === []) {
+    throw new RuntimeException(
+        sprintf('No template definition files found in directory: [%s].', $baseDir),
+    );
+}
 
 $templates = [];
 $seenKeys = [];
 
 /** @var string $file */
-foreach (glob($baseDir . '/*.php') as $file) {
+foreach ($files as $file) {
     $definition = require $file;
 
     if (!is_array($definition)) {
-        throw new \RuntimeException(
+        throw new RuntimeException(
             sprintf('Template file [%s] must return an array definition.', $file),
         );
     }
@@ -28,13 +42,13 @@ foreach (glob($baseDir . '/*.php') as $file) {
     $key = $definition['key'] ?? null;
 
     if (!is_string($key) || $key === '') {
-        throw new \RuntimeException(
+        throw new RuntimeException(
             sprintf('Template file [%s] must define a non-empty "key".', $file),
         );
     }
 
     if (isset($seenKeys[$key])) {
-        throw new \RuntimeException(
+        throw new RuntimeException(
             sprintf(
                 'Duplicate template key [%s] detected in [%s] and [%s].',
                 $key,
