c4daac4413a5f94331f8d752968b38c1da9c3445
Felipe Ruiz Terrazas <feliperterrazas@gmail.com>
2026-01-19 20:59:05 -0300

    feat(navigation): export module public api via index

---
+++ resources/js/Navigation/index.ts
@@ -0,0 +1,13 @@
+// resources/js/Navigation/index.ts
+export { default as Navigation } from './Navigation';
+
+export type {
+    AuthUser,
+    NavigationGroupItem,
+    NavigationItem,
+    NavigationLinkItem,
+    NavigationProps,
+    NavigationSectionItem,
+    SectionPosition,
+    SectionTargetNode,
+} from './types';
