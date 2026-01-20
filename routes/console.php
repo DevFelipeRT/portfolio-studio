0d355b17213de9e502f428ec9c514116a0f6ca21
Felipe Ruiz Terrazas <feliperterrazas@gmail.com>
2026-01-20 20:44:39 -0300

    fix(schedule): register images orphan cleanup using Schedule instance

--- routes/console.php
+++ routes/console.php
@@ -8,7 +8,8 @@ Artisan::command('inspire', function () {
     $this->comment(Inspiring::quote());
 })->purpose('Display an inspiring quote');
 
-Schedule::command('images:prune-orphans --disk=public --path=images --min-age-minutes=1440')
+app(Schedule::class)
+    ->command('images:prune-orphans --disk=public --path=images --min-age-minutes=1440')
     ->dailyAt('00:00')
     ->timezone('UTC')
     ->withoutOverlapping();
