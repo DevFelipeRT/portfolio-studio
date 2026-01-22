db142bf034b1f130fc4771a270cea739865d66c7
Felipe Ruiz Terrazas <feliperterrazas@gmail.com>
2026-01-22 10:05:32 -0300

    refactor(profile): move inertia page into module

--- resources/js/Modules/Profile/Pages/Edit.tsx
+++ resources/js/Modules/Profile/Pages/Edit.tsx
@@ -1,9 +1,9 @@
 import AuthenticatedLayout from '@/Layouts/AuthenticatedLayout';
 import { PageProps } from '@/types';
 import { Head } from '@inertiajs/react';
-import DeleteUserForm from './Partials/DeleteUserForm';
-import UpdatePasswordForm from './Partials/UpdatePasswordForm';
-import UpdateProfileInformationForm from './Partials/UpdateProfileInformationForm';
+import DeleteUserForm from '@/Modules/Profile/ui/DeleteUserForm';
+import UpdatePasswordForm from '@/Modules/Profile/ui/UpdatePasswordForm';
+import UpdateProfileInformationForm from '@/Modules/Profile/ui/UpdateProfileInformationForm';
 
 export default function Edit({
     mustVerifyEmail,
