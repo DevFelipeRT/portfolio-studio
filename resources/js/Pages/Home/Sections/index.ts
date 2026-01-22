a4978deba739811073ce6f623065754c261ab902
Felipe Ruiz Terrazas <feliperterrazas@gmail.com>
2026-01-22 10:22:44 -0300

    chore(home): remove legacy Pages directory

--- resources/js/Pages/Home/Sections/index.ts
+++
@@ -1,6 +0,0 @@
-export { ContactSection } from './ContactSection';
-export { ExperienceSection } from './ExperienceSection';
-export { HeroSection } from './HeroSection';
-export { HighlightsSection } from './HighlightsSection';
-export { ProjectSection } from './ProjectSection';
-export { TechStackSection } from './TechStackSection';
