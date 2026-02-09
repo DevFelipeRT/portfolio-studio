2925f36cc968badad17bb0df15aefbd76f9ab1c5
DevFelipeRt <feliperterrazas@gmail.com>
2026-02-09 14:27:37 -0300

    refactor(content-management): drop legacy feature re-exports

--- resources/js/Modules/ContentManagement/features/sections/index.ts
+++
@@ -1,3 +0,0 @@
-// Backwards-compatible re-export. Prefer importing from:
-//   "@/Modules/ContentManagement/features/page-rendering"
-export * from '@/Modules/ContentManagement/features/page-rendering';
--- resources/js/Modules/ContentManagement/features/templates/index.ts
+++
@@ -1,3 +0,0 @@
-// Backwards-compatible re-export. Prefer importing from:
-//   "@/Modules/ContentManagement/features/page-management/section/field-renderer"
-export * from '@/Modules/ContentManagement/features/page-management/section/field-renderer';
