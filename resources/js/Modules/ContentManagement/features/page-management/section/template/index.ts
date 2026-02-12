5006936518d9a7fd5afbb5b279f4125e7c1f9914
DevFelipeRt <feliperterrazas@gmail.com>
2026-02-12 15:53:51 -0300

    refactor(content-management/template): move TemplateSelector to module root and keep public export stable

--- resources/js/Modules/ContentManagement/features/page-management/section/template/index.ts
+++ resources/js/Modules/ContentManagement/features/page-management/section/template/index.ts
@@ -1,2 +1,2 @@
-export { TemplateSelector } from '@/Modules/ContentManagement/features/page-management/section/template/components/TemplateSelector';
+export { TemplateSelector } from '@/Modules/ContentManagement/features/page-management/section/template/TemplateSelector';
 export { TemplateForm } from '@/Modules/ContentManagement/features/page-management/section/template/TemplateForm';
