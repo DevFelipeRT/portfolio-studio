a223f67c8758dcdd155a4073f224f3e8a4a3bd47
DevFelipeRt <feliperterrazas@gmail.com>
2026-02-21 12:03:28 -0300

    chore(seeders): register module seeders in DatabaseSeeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,9 +2,10 @@
 
 namespace Database\Seeders;
 
-use App\Models\User;
+use App\Modules\IdentityAccess\Domain\Models\User;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
 
 class DatabaseSeeder extends Seeder
 {
@@ -15,12 +16,25 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // User::factory(10)->create();
+        User::query()->firstOrCreate(
+            ['email' => 'admin@example.com'],
+            [
+                'name' => 'Admin',
+                'password' => Hash::make('password'),
+                'email_verified_at' => now(),
+            ],
+        );
 
-        User::factory()->create([
-            'name' => 'Admin',
-            'email' => 'admin@example.com',
-            'password' => 'password'
+        $this->call([
+            WebsiteSettingsSeeder::class,
+            SkillsSeeder::class,
+            CoursesSeeder::class,
+            ContactChannelsSeeder::class,
+            ExperiencesSeeder::class,
+            ProjectsSeeder::class,
+            InitiativesSeeder::class,
+            ContentManagementSeeder::class,
+            MailSeeder::class,
         ]);
     }
 }
