979b41ea7a190080209d7de384fab0e160506e4f
DevFelipeRt <feliperterrazas@gmail.com>
2026-03-03 10:53:35 -0300

    refactor(i18n)!: make common i18n exports explicit
    Replace wildcard exports with a curated public API to stabilize imports.
    
    BREAKING CHANGE: '@/common/i18n' no longer re-exports all symbols from ./core and ./react; update imports to use the explicit exports.

--- resources/js/common/i18n/index.ts
+++ resources/js/common/i18n/index.ts
@@ -1,4 +1,21 @@
-export * from './namespaces';
-export * from './core';
-export * from './react';
-export { catalogProvider, createI18nEnvironment, translatorProvider } from './environment';
+export { I18N_NAMESPACES } from './namespaces';
+
+export {
+  catalogProvider,
+  createI18nEnvironment,
+  translatorProvider,
+} from './environment';
+
+export { createTranslatorProviderFromLoaders } from './core/translation';
+export type { TranslationModuleLoaders } from './core/translation';
+export type { PlaceholderValues } from './core/types';
+
+export {
+  I18nContext,
+  I18nProvider,
+  LanguageSelector,
+  LocaleSwitcher,
+  useGetLocale,
+  useSupportedLocales,
+  useTranslation,
+} from './react';
