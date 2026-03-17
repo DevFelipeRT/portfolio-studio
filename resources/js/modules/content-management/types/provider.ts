931d33677a74868b7377d38e9f1b31505b072836
Felipe <feliperterrazas@gmail.com>
2026-03-17 15:47:41 -0300

    fix(content-management): restore i18n support in component registry provider type

--- resources/js/modules/content-management/types/provider.ts
+++ resources/js/modules/content-management/types/provider.ts
@@ -1,5 +1,6 @@
 export interface ComponentRegistryProvider<
   TRegistry extends Record<string, unknown> = Record<string, unknown>,
 > {
+  i18n?: readonly string[];
   getSectionRegistry(): TRegistry;
 }
